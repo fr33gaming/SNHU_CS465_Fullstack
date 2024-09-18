2d259d78b5873007025b0f212425298fddc27b3b
Alexandro Hernandez <fr33gaming209@gmail.com>
2024-09-17 23:34:22 -0700

    Module 2 Updated Baseline

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
-const { handlebars } = require('hbs');
+var handlebars  = require('hbs');
 
 var app = express();
 
