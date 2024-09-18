1a770e77d96208a966659c04d4c53c6903b1cbbe
Alexandro Hernandez <fr33gaming209@gmail.com>
2024-09-17 21:29:51 -0700

    Module 2 Updated baseline

--- app.js
+++ app.js
@@ -6,11 +6,17 @@ var logger = require('morgan');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
-var travelRouter = require('./app_server/routes/travel')
+var travelRouter = require('./app_server/routes/travel');
+const { handlebars } = require('hbs');
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
+
+// register handlebars partials (https://www.npmjs.com/package/hbs)
+handlebars.registerPartials(__dirname + '/app_server/views/partials');
+
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
